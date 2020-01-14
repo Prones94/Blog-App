3ec9a6fa83f9586a74e2dd81213fe16e80565062
Ian Rones <ronesp.85@gmail.com>
2020-01-14 09:53:34 -0800

    [add] edit, update, and destroy functions inside app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require('express'),
-     mongoose = require('mongoose'),
-   bodyParser = require('body-parser'),
-         port = 3000,
-          app = express(),
-       Schema = mongoose.Schema;
+const  express = require('express'),
+      mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+methodOverride = require('method-override'),
+expressSanitizer = require('express-sanitizer'),
+          port = 3000,
+           app = express(),
+        Schema = mongoose.Schema;
+
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost:27017/blog_app", {
@@ -15,6 +18,8 @@ mongoose.connect("mongodb://localhost:27017/blog_app", {
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 // MONGOOSE/MODEL CONFIG
 let blogSchema = new Schema({
@@ -51,10 +56,13 @@ app.get('/blogs',(req,res) => {
 // NEW ROUTE
 app.get('/blogs/new', (req,res) => {
     res.render('new');
-})
+});
+
 // CREATE ROUTE
 app.post('/blogs', (req,res) =>{
     //create blog (DB.create(data, callback(error, new variable) =>{if(err){console.log('Error!,err)}else{}}))
+    console.log(req.body)
+    req.body.blog.body = req.sanitize(req.body.blog.body)
     Blog.create(req.body.blog, (err,newBlog) =>{
         if(err){
             res.render("new")
@@ -71,6 +79,42 @@ app.get("/blogs/:id", (req, res) =>{
             res.redirect("/blogs");
         } else {
             res.render("show", {blog: foundBlog});
+        }
+    })
+});
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", (req, res) => {
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        if(err){
+            res.redirect("/blogs")
+        } else {
+            res.render("edit", {blog:foundBlog});
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", (req,res) =>{
+    req.body.blog.body = req.sanitize(req.body.blog.body)
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            console.log(err)
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id)
+        }
+    })
+});
+
+// DELETE ROUTE
+app.delete("/blogs/:id", (req, res) =>{
+    // Destroy blog
+    Blog.findByIdAndRemove(req.params.id, (err) =>{
+        if(err) {
+            res.redirect('/blogs')
+        } else {
+            res.redirect('/blogs')
         }
     })
 })
